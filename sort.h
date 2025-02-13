6d3a682333c37a058e863b41104944c5723fae45
casey paul <casey.paul@atlasschool.com>
2025-02-13 17:36:46 +0000

    task 2 done, maybe

--- sort.h
+++ sort.h
@@ -23,9 +23,13 @@ typedef struct listint_s
 
 /* PRINT Prototypes functions */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 /* Atlas_sorting_arrays */
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge_sort_recursive(int *array, int left, int right);
+void merge(int *array, int left, int middle, int right);
 
 #endif /* SORT_H */
